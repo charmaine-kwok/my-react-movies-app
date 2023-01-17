86a6d7d04f2c22e41462f633d4a93653ba6bdcc5
Charmaine Kwok <charmaine@astri.org>
2023-01-17 10:39:57 +0800

    Add movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,21 @@
-import styles from "./Movies.module.css";
-import soul from "./img/yan/soul.jpeg";
-import anotherRound from "./img/yan/another-round.jpeg";
-import theresNoEvil from "./img/yan/there-is-no-evil.jpeg";
-import time from "./img/yan/time.jpeg";
-import firstGirlILoved from "./img/yan/first-girl-i-loved.jpeg";
+import styles from './Movies.module.css';
+import soul from './img/yan/soul.jpeg';
+import anotherRound from './img/yan/another-round.jpeg';
+import theresNoEvil from './img/yan/there-is-no-evil.jpeg';
+import time from './img/yan/time.jpeg';
+import firstGirlILoved from './img/yan/first-girl-i-loved.jpeg';
+import handRolledCigaratte from './img/yan/hand-rolled-cigaratte.JPG';
+import theFrenchDispatch from './img/yan/french-dispatch.jpeg';
+import yueLao from './img/yan/yue-lao.jpeg';
+import driveMyCar from './img/yan/drive-my-car.jpeg';
+import flee from './img/yan/flee.jpeg';
+import everythingEverywhereAllAtOnce from './img/yan/everything-everywhere-all-at-once.jpeg';
+import quoVadisAida from './img/yan/quo-vadis-aida.jpeg';
+import elvis from './img/yan/elvis.jpeg';
+import broker from './img/yan/broker.jpeg';
+import tableForSix from './img/yan/table-for-six.jpeg';
 
-import MoviesItem from "./MovieItem";
+import MoviesItem from './MovieItem';
 
 const Movies = () => {
   return (
@@ -49,6 +59,90 @@ const Movies = () => {
         location="D2 Place"
         date="3-Dec-2021"
         rating="6.0"
+      />
+      <MoviesItem
+        pic={handRolledCigaratte}
+        title="手捲煙"
+        desc="Okay movie."
+        location="D2 Place(?)"
+        date="3-Dec-2021(?)"
+        rating="7.5"
+      />
+      <MoviesItem
+        pic={theFrenchDispatch}
+        title="The French Dispatch"
+        desc="The cinematography is nice."
+        location="The One"
+        date="12-Dec-2021"
+        rating="7.5"
+      />
+      <MoviesItem
+        pic={yueLao}
+        title="月老"
+        desc={`
+        Okay movie. Some part a bit kam.
+        Actor and actresses are good looking.`}
+        location="Movie Town"
+        date="25-Dec-2021"
+        rating="7.5"
+      />
+      <MoviesItem
+        pic={driveMyCar}
+        title="Drive My Car"
+        desc="Longggggg"
+        location="Cinematheque"
+        date="27-Dec-2021"
+        rating="7.0"
+      />
+      <MoviesItem
+        pic={flee}
+        title="Flee"
+        desc="Movie is good."
+        location="Cinematheque"
+        date="4-June-2022"
+        rating="8.0"
+      />
+      <MoviesItem
+        pic={everythingEverywhereAllAtOnce}
+        title="Everything Everywhere All at Once"
+        desc="Well…. Interesting"
+        location="D Park"
+        date="15-June-2022"
+        rating="7.5"
+      />
+      <MoviesItem
+        pic={quoVadisAida}
+        title="Quo Vadis, Aida?"
+        desc="Okay movie."
+        location="K11 ART HOUSE"
+        date="26-June-2022"
+        rating="7.5"
+      />
+      <MoviesItem
+        pic={elvis}
+        title="Elvis"
+        desc="I like Elvis!
+        剩係聽歌都正！"
+        location="Festival Grand"
+        date="9-July-2022"
+        rating="8.0"
+      />
+      <MoviesItem
+        pic={broker}
+        title="孩子轉運站"
+        desc="Okay movie.
+        Fell asleep again…"
+        location="Cinematheque"
+        date="23-July-2022"
+        rating="7.5"
+      />
+      <MoviesItem
+        pic={tableForSix}
+        title="飯戲攻心"
+        desc="Funny movie."
+        location="Paris London New York Milano"
+        date="24-Sep-2022"
+        rating="8.5"
       />
     </div>
   );
--- src/components/NavBar/Profile.js
+++ src/components/NavBar/Profile.js
@@ -1,25 +1,25 @@
-import styles from "./Profile.module.css";
-import ball from "./ball.png";
+import styles from './Profile.module.css';
+import ball from './ball.png';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const moon = <FontAwesomeIcon icon={faMoon} color="gold" />;
 const sun = <FontAwesomeIcon icon={faSun} color="gold" />;
 
 const Profile = (props) => {
   return (
-    <div className={styles["profile-container"]}>
-      <img className={styles["profile-picture"]} src={ball} alt="" />
-      <div className={styles["profile-text-container"]}>
-        <span className={styles["profile-text"]}>Profile</span>
-        <i className={styles["fa-solid fa-caret-down"]}></i>
+    <div className={styles['profile-container']}>
+      <img className={styles['profile-picture']} src={ball} alt="" />
+      <div className={styles['profile-text-container']}>
+        <span className={styles['profile-text']}>Profile</span>
+        <i className={styles['fa-solid fa-caret-down']}></i>
       </div>
       <div className={styles.toggle}>
         <div>{moon}</div>
         <div>{sun}</div>
         <div
-          className={styles["toggle-ball"]}
+          className={styles['toggle-ball']}
           onClick={props.onChangeTheme}
         ></div>
       </div>
